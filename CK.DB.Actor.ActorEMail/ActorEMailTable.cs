2655d922354bfbc8097f349fc2d872fc4d873699
Olivier Spinelli <olivier.spinelli@signature-code.com>
2019-08-15 23:54:01 +0200

    Upgraded CK-Database.

--- CK.DB.Actor.ActorEMail/ActorEMailTable.cs
+++ CK.DB.Actor.ActorEMail/ActorEMailTable.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using CK.SqlServer.Setup;
-using CK.Setup;
+using CK.Core;
 using CK.SqlServer;
 using System.Threading.Tasks;
 
